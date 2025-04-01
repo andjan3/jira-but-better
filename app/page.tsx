4214b5b13c3be60c2c2fab2d775032e61e5088cc
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-01 09:11:00 +0200

    fixed sign in and sign out, also implemented a welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,23 @@
-"use cli";
-
 import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth";
-import { User } from "./user";
-import { LoginButton, LogoutButton } from "./auth";
-import { FaRegUser } from "react-icons/fa6";
 import { UserIcon } from "@/components/ui/user-icon";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <nav className="bg-black h-[100px] w-full flex items-center p-4">
-      <div className="grid grid-cols-3 w-full">
-        <div className="text-white">Jira but better</div>
-      </div>
+    <>
+      <nav className="bg-black h-[100px] w-full flex flex-row items-center p-4 px-10 justify-between">
+        <div>
+          <div className="text-white">Jira but better</div>
+        </div>
 
-      <UserIcon />
+        {session && session.user?.name && (
+          <div className="text-white">{`Welcome: ${session.user.name}`}</div>
+        )}
 
-      {/* <LoginButton />
-      <LogoutButton />
-      <h2>Server Session</h2>
-      <pre>{JSON.stringify(session)}</pre>
-
-      <h2>Client session</h2> */}
-      <User />
-    </nav>
+        <UserIcon props={session ? true : false} />
+      </nav>
+    </>
   );
 }
